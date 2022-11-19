61a915295d203e8b31e01a2dc4e7898bbc9b2dcf
fakerr-sketch <fm3209742@gmail.com>
2022-11-19 11:19:37 -0300

    html:template:add

---
+++ includes/htmlTemplate.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../static/custom.css"> 
+    <title>Pw Quiz</title>
+</head>
+
+<body>
+</body>
+</html>
