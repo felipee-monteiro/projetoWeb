130f854e157a8677f6bf031d1b201ed64ce5c148
Gabi <gabrielacavalcantee@hotmail.com>
2022-11-18 22:24:01 -0300

    Hey, baby! I'm comming!

--- views/index.php
+++ views/index.php
@@ -5,16 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" />
-    <title>Document</title>
+    <title>Pw Quiz</title>
 </head>
 <body>
     <div class="px-4 pt-5 my-5 text-center border-bottom">
-    <h1 class="display-4 fw-bold">Centered screenshot</h1>
+    <h1 class="display-4 fw-bold">Pw Quiz</h1>
     <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+      <p class="lead mb-4">Um mundo onde a curiosidade é a sua melhor característica. Participe de quizzes relacionados a todos os tipos de temas imagináveis!</p>
       <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-        <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3">Primary button</button>
-        <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+        <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3">Login</button>
+        <button type="button" class="btn btn-outline-secondary btn-lg px-4">Continuar sem logar</button>
       </div>
     </div>
     <div class="overflow-hidden" style="max-height: 30vh;">
