fa83f2704c17d8e77c90720f69c09703a0158d96
fakerr-sketch <fm3209742@gmail.com>
2022-11-19 11:17:40 -0300

    database:connection

---
+++ database/connection.php
@@ -0,0 +1,9 @@
+<?php
+
+    $hostname = "localhost";
+    $username = "root";
+    $dbname = "quiz";
+    
+    $connection = mysqli_connect($hostname, $username, null, $dbname);
+    $isConnected = $connection->error;
+    
