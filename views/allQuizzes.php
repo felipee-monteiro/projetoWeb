a6735a60f315a4c601c6ffb8187bba2f6a3c1f62
fakerr-sketch <fm3209742@gmail.com>
2022-11-21 09:27:12 -0300

    screen:finished

--- views/allQuizzes.php
+++ views/allQuizzes.php
@@ -1,14 +1,39 @@
 <?php 
     require_once "../database/connection.php";
     require_once "../database/quizzes.php";
-    require_once "../includes/htmlTemplate.php";
+    include_once "../includes/htmlTemplate.php";
 
-    $quizes = getQuizData($connection, "SELECT data FROM quiz_data");
+    $quizContentAsArray = array();
+    $quizes = getQuizData($connection, "SELECT id, data FROM quiz_data");
     mysqli_close($connection);
 
     if ($quizes) {
         foreach ($quizes as $quiz) {
-            echo $quiz;
+           $quizId = $quiz['id']; 
+           $quiz = json_decode($quiz['data']);
+           array_push($quizContentAsArray, "
+                     <div class='card m-2' style='width: 18rem;'>
+                        <div class='card-body'>
+                            <h5 class='card-title'>$quiz->title</h5>
+                            <h6 class='card-subtitle mb-2 text-muted'>$quiz->theme</h6>
+                            <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href='quiz.php?quiz_id=$quizId' class='card-link'>Responder</a>
+                        </div>
+                    </div>
+            ");
         }
+        $quizContentAsString = implode(" ", $quizContentAsArray);
+        generateTemplate("
+            <div class='d-flex flex-wrap justify-content-center align-items-center my-4'>
+                $quizContentAsString
+            </div>
+        ");
+    } else {
+        generateTemplate('
+          <div class="text-center">
+            Error 404
+          </div>
+        ');
     }
 ?>
+    
