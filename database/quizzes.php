c2ea6a42460041f734b997df75331ddbe40f6831
fakerr-sketch <fm3209742@gmail.com>
2022-11-21 09:27:48 -0300

    code:refactoring

--- database/quizzes.php
+++ database/quizzes.php
@@ -1,11 +1,9 @@
 <?php
 
-require_once "connection.php";
-
 function getQuizData($conn, $query){
     $data = mysqli_query($conn, $query);
     if ($data->num_rows > 0) {
-        $dataAsArray = mysqli_fetch_array($data);
+        $dataAsArray = mysqli_fetch_all($data, MYSQLI_ASSOC);
         return $dataAsArray;
     }
     return false;
