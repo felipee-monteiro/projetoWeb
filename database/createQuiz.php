b5dadebb66ec934d8fc622977b6d5e17f4ede316
fakerr-sketch <fm3209742@gmail.com>
2022-11-22 11:11:10 -0300

    file:createQuiz:create

---
+++ database/createQuiz.php
@@ -0,0 +1,11 @@
+<?php
+
+    // if (isset($_POST['questions'], $_POST['title'], $_POST['tema'])) {
+    //     $title = $_POST['title'];
+    //     $theme = $_POST['tema'];
+    //     $questions = $_POST['questions'];
+
+        print_r($_POST);
+    // }
+
+?>
