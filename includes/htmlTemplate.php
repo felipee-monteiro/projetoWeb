022cb4496e0087f136e875b437683aea917d8f27
fakerr-sketch <fm3209742@gmail.com>
2022-11-22 11:12:09 -0300

    function:generateTemplate:increase

--- includes/htmlTemplate.php
+++ includes/htmlTemplate.php
@@ -1,22 +1,46 @@
 <?php
 
-function generateTemplate($content){
-    echo '
-        <!DOCTYPE html>
-        <html lang="pt-BR">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" />
-            <link rel="stylesheet" href="../static/custom.css"> 
-            <title>Pw Quiz</title>
-        </head>
-        <body>' 
-        .$content.'
-        </body>
-        </html>
-    ';
+function generateTemplate($content, $shouldHaveScript = false, $script = ""){
+    
+    if ($shouldHaveScript && $script) {     
+        echo "
+            <!DOCTYPE html>
+            <html lang='pt-BR'>
+            <head>
+                <meta charset='UTF-8'>
+                <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+                <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' />
+                <link rel='stylesheet' href='../static/custom.css'> 
+                <title>Pw Quiz</title>
+            </head>
+            <body>
+            $content
+            <script>
+                $script
+            </script>
+            </body>
+            </html>
+        ";
+    } else {
+        echo "
+            <!DOCTYPE html>
+            <html lang='pt-BR'>
+            <head>
+                <meta charset='UTF-8'>
+                <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+                <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' />
+                <link rel='stylesheet' href='../static/custom.css'> 
+                <title>Pw Quiz</title>
+            </head>
+            <body>
+            $content
+            </body>
+            </html>
+        ";
+    }
+
 }
 
 ?>
