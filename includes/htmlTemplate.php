3a5fa7e8802de08fc42e4baf0f4ce4b79a1803c3
fakerr-sketch <fm3209742@gmail.com>
2022-11-24 07:48:32 -0300

    css:update

--- includes/htmlTemplate.php
+++ includes/htmlTemplate.php
@@ -10,7 +10,6 @@ function generateTemplate($content, $shouldHaveScript = false, $script = ""){
                 <meta http-equiv='X-UA-Compatible' content='IE=edge'>
                 <meta name='viewport' content='width=device-width, initial-scale=1.0'>
                 <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' />
-                <link rel='stylesheet' href='../static/custom.css'> 
                 <title>Pw Quiz</title>
             </head>
             <body>
@@ -30,7 +29,6 @@ function generateTemplate($content, $shouldHaveScript = false, $script = ""){
                 <meta http-equiv='X-UA-Compatible' content='IE=edge'>
                 <meta name='viewport' content='width=device-width, initial-scale=1.0'>
                 <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' />
-                <link rel='stylesheet' href='../static/custom.css'> 
                 <title>Pw Quiz</title>
             </head>
             <body>
